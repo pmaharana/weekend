8d7c407f4d1ea0ad6177b3e62aebdb4889e589e1
Pranay Maharana <pranay.maharana@gmail.com>
2017-03-23 16:07:05 -0400

    --setup is done

---
+++ blackjack/blackjack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var deck = new List<Card>();
+
+            foreach (Rank r in Enum.GetValues(typeof(Rank)))
+            {
+                foreach (Suit s in Enum.GetValues(typeof(Suit)))
+                {
+                    deck.Add(new Card(s, r));
+                }
+            }
+
+            var randomDeck = deck.OrderBy(x => Guid.NewGuid()).ToList();
+
+            
+
+        }
+    }
+}
---
+++ blackjack/blackjack/Rank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    public enum Rank
+    {
+            Ace,
+            Deuce,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+    }
+}
