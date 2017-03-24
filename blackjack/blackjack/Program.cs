9bbfd26692adf1b6dc8f9b0b997cf538cf3e0597
Pranay Maharana <pranay.maharana@gmail.com>
2017-03-24 19:38:43 -0400

    --kinda done, need to fix some bugs

--- blackjack/blackjack/Program.cs
+++ blackjack/blackjack/Program.cs
@@ -8,9 +8,8 @@ namespace blackjack
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<Card> CreateAndShuffleDeck()
         {
-
             var deck = new List<Card>();
 
             foreach (Rank r in Enum.GetValues(typeof(Rank)))
@@ -19,41 +18,157 @@ namespace blackjack
                 {
                     deck.Add(new Card(s, r));
                 }
+}
+
+            var randomDeck = deck.OrderBy(x => Guid.NewGuid()).ToList();
+
+            return randomDeck;
+        }  //create and shuffle deck, ready to draw 
+
+        //deal cards to player
+        static List<Card> DealPlayerCards(List<Card> randomDeck, List<Card> playerHand)
+        {
+            playerHand.Add(randomDeck[0]); 
+            Console.WriteLine($"You drew the {randomDeck[0]}"); 
+            randomDeck.RemoveAt(0);
+            return playerHand;
+        }
+
+        //deal cards to dealer
+        static List<Card> DealDealerCards(List<Card> randomDeck, List<Card> dealerHand)
+        {
+            dealerHand.Add(randomDeck[0]);
+            Console.WriteLine($"The dealer drew {randomDeck[0]}");
+            randomDeck.RemoveAt(0);
+            return dealerHand;
+        }
+
+        //get the total of the hand by converting card class to int
+        static int GetHandTotal(List<Card> hand) 
+        {
+            var sum = 0;
+            foreach (var card in hand)
+            {
+                sum += card.GetCardValue();
             }
+            Console.WriteLine($"Total points: {sum} points");
+            Console.WriteLine(" ");
+            return sum;
+        }
 
-            var randomDeck = deck.OrderBy(x => Guid.NewGuid()).ToList(); //cards are in random order in deck
 
-            //Greetings Message goes here 
+            
+
+
+        //Hit or stand response from user. Coded to only accept a number 
+        static int HitOrStand(string message) 
+        {
+            var input = message;
+            int number = 0;
+            bool wasFormatCorrect = int.TryParse(input, out number);
+            while (!wasFormatCorrect)
+            {
+                Console.WriteLine("Please enter the number 1 or 2, do not use words");
+                input = Console.ReadLine();
+                wasFormatCorrect = int.TryParse(input, out number);
+            }
+            return number;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            //cards are in random order in deck
+
+            // Greetings Message goes here
 
             //Draw a card from deck
-            var discardPile = new List<Card>();
+
+            var randomDeck = CreateAndShuffleDeck(); //shuffle deck and declare to a variable 
+
             int count = 0;
             var playerHand = new List<Card>();
-            var playerPoints = new List<int>();
             var playerTotal = 0;
-            while (count < 5 && playerTotal < 21)
+            var hitOrStand = 0;
+            var dealerHand = new List<Card>();
+            var dealerTotal = 0;
+            bool stand = true;
+
+
+            while (count < 5 && playerTotal < 22 && stand == true)
             {
-                Console.WriteLine($"The drawn card is {randomDeck[0]}"); //drawing first card from top
-                playerHand.Add(randomDeck[0]); //adds the top card to player's hand list & removes it
-                randomDeck.RemoveAt(0);
-                Console.WriteLine("  ");
-                Console.WriteLine($"The value of the card:{playerHand[count].GetCardValue()}, "); //card points 
-                playerPoints.Add(playerHand[count].GetCardValue()); //adds players points to another list
-                playerTotal = playerPoints.Sum(); //takes the total of player's points so far
-                Console.WriteLine("");
-                Console.WriteLine($"The total number of points in your hands: {playerTotal}");
-                if (playerTotal < 21)
+                for (int i = 0; i < 2; i++)
                 {
-                    Console.WriteLine("Want me to hit you, or do you wanna fold?");
-                    Console.WriteLine(" ");
+                DealPlayerCards(randomDeck, playerHand); //deal 1 card to player and remove from deck
+                playerTotal = GetHandTotal(playerHand); //calculates the sum so far of the player hand
+                
+                    if (dealerTotal < 16)
+                    {
+                        DealDealerCards(randomDeck, dealerHand); //dealer draws next card & remove from deck 
+                        dealerTotal = GetHandTotal(dealerHand); //calculates the sum so far of the dealer hand
+                    }
                 }
-                count++;
-                if (playerTotal > 21)
+
+
+                 Console.WriteLine("Enter 1 to hit or 2 to stand");
+                 hitOrStand = HitOrStand(Console.ReadLine());
+
+                if (hitOrStand == 1 && count > 2 && dealerTotal < 16 && playerTotal < 21)
+                 {
+                    DealPlayerCards(randomDeck, playerHand);
+                    playerTotal = GetHandTotal(playerHand);
+
+                    DealPlayerCards(randomDeck, dealerHand);
+                    dealerTotal = GetHandTotal(dealerHand);
+                }
+               
+                if (hitOrStand == 2)
                 {
-                    Console.WriteLine("You lose the game!");
+                    stand = false;
+                }
+                    
+                count++;
+                        
+            }
+
+            if (stand == false)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Let's compare cards");
+                if (playerTotal > dealerTotal)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Your points: " + playerTotal + " and Dealer's points: " + dealerTotal);
+                    Console.WriteLine("COngratulations, you win!");
+                }
+                else if (playerTotal < dealerTotal)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Your points: " + playerTotal + " and Dealer's points: " + dealerTotal);
+                    Console.WriteLine("You lose");
+                }
+                else if (playerTotal == dealerTotal)
+                {
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Your points: " + playerTotal + " and Dealer's points: " + dealerTotal);
+                    Console.WriteLine("It's a tie!");
                 }
             }
-                
+            else
+            {
+                Console.WriteLine("You lose!");
+            }
+
+
+            
+  
+
+
+
+
+
                 
             
           
